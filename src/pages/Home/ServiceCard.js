f181c8b8e4b726a4ce3d27a4c5bf52fc91d4c57a
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-05 23:04:12 +0300

    data loaded from the database

---
+++ src/pages/Home/ServiceCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ServiceCard = ({ service }) => {
+    const { title, img, price } = service;
+    return (
+        <div className="card  bg-base-100 shadow-xl">
+            <figure><img src={img} alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                <p className='text-2xl font-semibold text-orange-600'>Price: ${price}</p>
+                <div className="card-actions justify-end">
+                    <Link >
+                        <button className="btn btn-primary">Buy Now</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceCard;
--- src/pages/Home/Services.js
+++ src/pages/Home/Services.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import ServiceCard from './ServiceCard';
 
 const Services = () => {
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => {
+                setServices(data)
+            })
+    })
+
     return (
-        <div>
-            <p>this is services</p>
+        <div className='mb-10'>
+            <div className='text-center flex flex-col gap-2 mb-10'>
+                <p className="text-2xl font-bold text-orange-600">Services</p>
+                <p className="text-5xl font-semibold">Our Service Area</p>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8' >
+                {
+                    services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+                }
+            </div>
         </div>
     );
 };
