c8374bbd4e6caa00e1203aaa33b9e609711154c8
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-06 11:13:30 +0300

    data fetching completed for specific email

--- src/pages/Orders/Orders.js
+++ src/pages/Orders/Orders.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../contexts/AuthProvider';
 
 const Orders = () => {
+    const [orders, setOrders] = useState();
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/orders?email=${user?.email}`, {
+            method: 'GET',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }, [user?.email])
+
     return (
         <div>
             <p>this is orders</p>
---
+++ src/pages/Orders/OrdersCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrdersCard = () => {
+    return (
+        <div>
+            <p>this is orders card</p>
+        </div>
+    );
+};
+
+export default OrdersCard;
